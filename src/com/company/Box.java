e292572b061563ed7883a53a727ff0ac2ef809c4
NULLrider <65449313+RayCohle@users.noreply.github.com>
2021-01-03 15:12:18 +0200

    Created basic box

---
+++ src/com/company/Box.java
@@ -0,0 +1,47 @@
+package com.company;
+
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Box {
+    ArrayList<Integer> origin = new ArrayList<Integer>();
+    Box(ArrayList<Integer> origin)
+    {
+        this.origin = origin;
+    }
+    HashMap<InetAddress, ArrayList<Integer>> map = new HashMap<InetAddress, ArrayList<Integer>>();
+    boolean write(InetAddress address, int port)
+    {
+        if(map.containsKey(address))
+        {
+            map.get(address).add(port);
+
+            if(map.get(address).size()==origin.size())
+            {
+                for(int a = 0; a<origin.size(); a++)
+                {
+                    if(!map.get(address).get(a).equals(origin.get(a)))
+                    {
+                        map.put(address,new ArrayList<Integer>());
+                        return false;
+                    }
+                }
+                map.put(address,new ArrayList<Integer>());
+                return true;
+
+            }
+            else{
+                map.get(address).add(port);
+                return false;
+            }
+        }
+        else{
+            map.put(address,new ArrayList<Integer>());
+            map.get(address).add(port);
+        return false;
+
+        }
+
+    }
+}
